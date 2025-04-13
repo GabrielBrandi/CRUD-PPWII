4501578904dcd4345afb4f37cd9f5de6ec93ad8a
Gabriel Brandino <gabriel.brandino01@gmail.com>
2025-04-13 20:55:24 -0300

    feat(migrations): inserir scripts do projeto

--- migrations/0001 - CREATE_TABLE_LIVRO.sql
+++ migrations/0001 - CREATE_TABLE_LIVRO.sql
@@ -1,8 +1,11 @@
+CREATE DATABASE IF NOT EXISTS Biblioteca;
 USE Biblioteca;
 
 CREATE TABLE LIVRO (
     ID_Livro INT PRIMARY KEY AUTO_INCREMENT,
     Nome VARCHAR(150) NOT NULL,
     Paginas INT,
+    Emprestado CHAR(1),
+    Sinopse VARCHAR(150),
     Genero VARCHAR(50)
 );
--- migrations/0002 - CREATE_TABLE_EMPRESTIMO.sql
+++ migrations/0002 - CREATE_TABLE_EMPRESTIMO.sql
@@ -2,10 +2,8 @@ USE Biblioteca;
 
 CREATE TABLE EMPRESTIMO (
     ID_Emprestimo INT PRIMARY KEY AUTO_INCREMENT,
-    ID_Cliente INT,
     ID_Livro INT,
     Data_Emprestimo DATE NOT NULL,
     Data_Devolucao DATE,
-    FOREIGN KEY (ID_Cliente) REFERENCES CLIENTE(ID_Cliente),
     FOREIGN KEY (ID_Livro) REFERENCES LIVRO(ID_Livro)
 );
